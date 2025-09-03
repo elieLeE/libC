5e11bc3d63d3d279925e8532fb626ac7cdd9ce23
LI <duleu.elie@gmail.com>
2025-09-04 01:02:16 +0200

    Fix indent of fichier.c file

--- src/io/fichier.c
+++ src/io/fichier.c
@@ -3,8 +3,8 @@
 FILE* ouvFichier(char const *name, char const *mode){
     FILE* f = fopen(name, mode);
     if(f == NULL){
-	fprintf(stderr, "impossible d'ouvrir le fichier %s\n", name);
-	exit(0);
+        fprintf(stderr, "impossible d'ouvrir le fichier %s\n", name);
+        exit(0);
     }
     return f;
 }
@@ -16,8 +16,8 @@ void fermerFichier(FILE **f){
 void lireMatrice(FILE* fichier, unsigned int* matrice[], int const tailleLig, int const tailleCol){
     int i, j;
     for(i=0; i<tailleLig; i++){
-	for(j=0; j<tailleCol; j++){
-	    fscanf(fichier, "%d", &matrice[i][j]);
-	}
+        for(j=0; j<tailleCol; j++){
+            fscanf(fichier, "%d", &matrice[i][j]);
+        }
     }
 }
