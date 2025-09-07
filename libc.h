678a195bc61ec8007c6cdb4184bdaf9807195b06
LI <duleu.elie@gmail.com>
2025-09-08 00:35:49 +0200

    Create libc.h file summarizing all actual functionalities.
    Create this file in order to simplify the using of the library.
    If, in the future, a complex new functionalities are added, maybe it will
    not be added in this file.

---
+++ libc.h
@@ -0,0 +1,19 @@
+#ifndef __LIB_C_H__
+#define __LIB_C_H__
+
+#define __attr_unused__  __attribute__((unused))
+#define __attr_nonnull__  __attribute__((nonnull))
+
+
+#include "src/io/fichier.h"
+#include "src/mem/mem.h"
+#include "src/liste/liste.h"
+#include "src/swap.h"
+
+#include "src/math/sum.h"
+#include "src/math/prime.h"
+#include "src/math/nbre.h"
+
+#include "src/tab/tab.h"
+
+#endif
