7c04deac8880af45e1ae5704041b3db5923b0220
LI <duleu.elie@gmail.com>
2025-09-07 22:31:53 +0200

    Fix indent in mem/alloc.c.

--- src/mem/alloc.c
+++ src/mem/alloc.c
@@ -1,52 +1,70 @@
 #include "alloc.h"
 
-void* my_calloc(size_t const taille){
-    if(taille<=0){
-        fprintf(stderr, "taille <= 0, fichier %s, ligne %d\n", __FILE__, __LINE__);
+void* my_calloc(size_t const taille)
+{
+    void *p;
+
+    if(taille<=0) {
+        fprintf(stderr, "taille <= 0, fichier %s, ligne %d\n",
+                __FILE__, __LINE__);
         exit(0);
     }
-    void *p = calloc(1, taille);
-    if(p == NULL){
-        fprintf(stderr, "allocation incorrecte, fichier %s, ligne %d\n", __FILE__, __LINE__);
+
+    p = calloc(1, taille);
+    if(p == NULL) {
+        fprintf(stderr, "allocation incorrecte, fichier %s, ligne %d\n",
+                __FILE__, __LINE__);
         exit(0);
     }
     return p;
 }
 
-void* my_realloc(void *p, size_t const taille){
-    if(taille<=0){
-        fprintf(stderr, "taille <= 0, fichier %s, ligne %d\n", __FILE__, __LINE__);
+void* my_realloc(void *p, size_t const taille)
+{
+    if(taille<=0) {
+        fprintf(stderr, "taille <= 0, fichier %s, ligne %d\n",
+                __FILE__, __LINE__);
         exit(0);
     }
+
     p = realloc(p, taille);
-    if(p == NULL){
-        fprintf(stderr, "allocation incorrecte, fichier %s, ligne %d\n", __FILE__, __LINE__);
+    if(p == NULL) {
+        fprintf(stderr, "allocation incorrecte, fichier %s, ligne %d\n",
+                __FILE__, __LINE__);
         exit(0);
     }
     return p;
 }
 
-void** allocTab2D(int const lig, int const col, size_t const tailleType){
-    if((tailleType<=0) || (lig<=0) || (col<=0)){
-        fprintf(stderr, "taille, lig ou col <= 0, fichier %s, ligne %d\n", __FILE__, __LINE__);
+void** allocTab2D(int const lig, int const col, size_t const tailleType)
+{
+    int i, t;
+    void **tab;
+
+    if((tailleType<=0) || (lig<=0) || (col<=0)) {
+        fprintf(stderr, "taille, lig ou col <= 0, fichier %s, ligne %d\n",
+                __FILE__, __LINE__);
         exit(0);
     }
-    int i, t;
-    void **tab = my_calloc(lig*sizeof(void*));
-    t = col*tailleType;
-    for(i=0; i<lig; i++){
+
+    tab = my_calloc(lig * sizeof(void*));
+    t = col * tailleType;
+    for(i=0; i<lig; i++) {
         tab[i] = my_calloc(t);
     }
     return tab;
 }
 
-void liberationTab2D(void** matrice, int const lig){
-    if(lig<=0){
-        fprintf(stderr, "lig <= 0, fichier %s, ligne %d\n", __FILE__, __LINE__);
+void liberationTab2D(void** matrice, int const lig)
+{
+    int i;
+
+    if(lig<=0) {
+        fprintf(stderr, "lig <= 0, fichier %s, ligne %d\n",
+                __FILE__, __LINE__);
         exit(0);
     }
-    int i;
-    for(i=0; i<lig; i++){
+    for(i=0; i<lig; i++) {
         free(matrice[i]);
         matrice[i] = NULL;
     }
