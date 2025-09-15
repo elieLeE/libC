f193f5b70ac180f7ca7031b85c78998bb55bb638
LI <duleu.elie@gmail.com>
2025-09-15 17:40:32 +0200

    math/sum: fix a type error in the previous commit already pushed...

--- src/math/sum.c
+++ src/math/sum.c
@@ -5,7 +5,7 @@ unsigned long sum_1_to_n(unsigned long n)
     return n * (n + 1) / 2;
 }
 
-unsigned long sun_carre_1_to_n(unsigned long n)
+unsigned long sum_carre_1_to_n(unsigned long n)
 {
     return (n * (n + 1) * (2 * n + 1)) / 6;
 }
--- src/math/sum.h
+++ src/math/sum.h
@@ -2,6 +2,6 @@
 #define _SHARED_SUM_H_
 
 unsigned long sum_1_to_n(unsigned long n);
-unsigned long sun_carre_1_to_n(unsigned long n);
+unsigned long sum_carre_1_to_n(unsigned long n);
 
 #endif
