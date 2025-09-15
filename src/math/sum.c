27f77bdbf1028752a73903ec988b4801903dd103
LI <duleu.elie@gmail.com>
2025-09-15 17:32:29 +0200

    math/sum: just improve a little the name of the methods in sum.h

--- src/math/sum.c
+++ src/math/sum.c
@@ -1,11 +1,11 @@
 #include "sum.h"
 
-unsigned long sum1_to_n(unsigned long n)
+unsigned long sum_1_to_n(unsigned long n)
 {
     return n * (n + 1) / 2;
 }
 
-unsigned long sum_carre(unsigned long n)
+unsigned long sun_carre_1_to_n(unsigned long n)
 {
     return (n * (n + 1) * (2 * n + 1)) / 6;
 }
