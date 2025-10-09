1bce4d4ddfd79614ada219e84b022c06d83e330d
LI <duleu.elie@gmail.com>
2025-10-10 01:15:19 +0200

    src/logger: adds the color MAGENTA.
    This color will be used in the test.

--- src/logger/logger.h
+++ src/logger/logger.h
@@ -10,6 +10,7 @@
 #define COLOR_GREEN "\x1B[32m"
 #define COLOR_YELLOW "\x1B[33m"
 #define COLOR_BLUE "\x1B[34m"
+#define COLOR_MAGENTA "\x1B[35m"
 #define COLOR_WHITE "\x1B[37m"
 
 #define COLOR_RESET "\x1b[0m"
