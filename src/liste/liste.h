2ad8f4cdeb487267da661b9e0ee61a3dcc69e8b3
LI <duleu.elie@gmail.com>
2025-10-18 00:23:22 +0200

    src/liste: remove type.h file.
    Remove this file and move 'generic_liste_t' and 'generic_elem_liste_t'
    definitions in liste.h.

--- src/liste/liste.h
+++ src/liste/liste.h
@@ -9,7 +9,16 @@
  */
 
 #include <stdbool.h>
-#include "type.h"
+
+typedef struct generic_elem_liste_t {
+    struct generic_elem_liste_t *prec, *suiv;
+    void *data;
+} generic_elem_liste_t;
+
+typedef struct generic_liste_t {
+    generic_elem_liste_t *first, *end;
+    unsigned int nbre_elem;
+} generic_liste_t;
 
 /* Here, head has to be a generic_elem_liste_t */
 #define gl_for_each(_p, _head) \
