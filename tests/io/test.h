240885d493f63dc980ad873b8c46dda77b992d70
LI <duleu.elie@gmail.com>
2025-10-18 01:48:11 +0200

    test/io: move IO tests to main folder.

--- tests/io/test.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TEST_H_
-#define _TEST_H_
-
-void test_lecture_simple();
-void test_lecture_matrice();
-
-#endif
-
--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,10 @@
 #include "test_liste.h"
+#include "test_io.h"
 
 int main()
 {
     run_tests_liste();
+    run_tests_io();
 
     return 0;
 }
