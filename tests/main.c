a4153920648b3eac8c94fbb61d653f16980a3cad
LI <duleu.elie@gmail.com>
2025-10-18 02:08:40 +0200

    test/swap: move swap test in main folder.
    Interest of this test in NULL for now. But, it could be useful once SWAP
    macro will have been refactored in in order to be more generic.

--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,27 @@
+#include "../src/macros.h"
+
 #include "test_liste.h"
 #include "test_io.h"
 
+static void test_swap(void)
+{
+    int a = 5;
+    int b = 13;
+
+    SWAP(int, &a, &b);
+
+    ASSERT_EQUAL(a, 13);
+    ASSERT_EQUAL(b, 5);
+}
+
 int main()
 {
+    BEGIN_TEST_MODULE("WITHOUT MODULE");
+
+    CALL_TEST_FUNC(test_swap);
+
+    END_TEST_MODULE();
+
     run_tests_liste();
     run_tests_io();
 
