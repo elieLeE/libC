9ebf01ab899ede17904816265ce9ecc01971ecc1
LI <duleu.elie@gmail.com>
2025-11-12 16:07:41 +0100

    tests/vector: rename 'test_add_elem_last' test.
    Rename this test as 'test_fill_vector'.

--- tests/test_vector.c
+++ tests/test_vector.c
@@ -2,22 +2,9 @@
 #include "../src/vector/vector.h"
 #include "../src/macros.h"
 
-static void check_vector_data(const gv_t(int32) *vec,
-                              const int32_t expected_vals[],
-                              int expected_vals_count)
+static void test_fill_vector(void)
 {
     int idx_tab = 0;
-
-    ASSERT_EQUAL(vec->len, expected_vals_count);
-
-    gv_for_each_pos(pos, vec) {
-        ASSERT_EQUAL(vec->tab[pos], expected_vals[idx_tab]);
-        idx_tab++;
-    }
-}
-
-static void test_add_elem_last(void)
-{
     gv_t(int32) vector;
     int32_t tab[5] = {1, 2, 3, 4, 5};
 
@@ -29,9 +16,14 @@ static void test_add_elem_last(void)
     gv_add_elem_last(&vector, 4);
     gv_add_elem_last(&vector, 5);
 
-    check_vector_data(&vector, tab, 5);
+    ASSERT_EQUAL(vector.len, 5);
 
-    gv_free(&vector, NULL);
+    gv_for_each_pos(pos, &vector) {
+        ASSERT_EQUAL(vector.tab[pos], tab[idx_tab]);
+        idx_tab++;
+    }
+
+    gv_wipe(&vector, NULL);
 }
 
 
@@ -40,7 +32,7 @@ module_tests_t *get_all_tests_vector(void)
     module_tests_t *module_tests = RETHROW_P(p_calloc(sizeof(module_tests_t)));
 
     set_module_name(module_tests, "VECTOR");
-    ADD_TEST_TO_MODULE(module_tests, test_add_elem_last);
+    ADD_TEST_TO_MODULE(module_tests, test_fill_vector);
 
     return module_tests;
 }
