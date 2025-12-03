8844500e8da1714b1d7577c632e09610b6d7d303
LI <duleu.elie@gmail.com>
2025-12-04 00:59:11 +0100

    test/prime: check the divisors of N.
    Add a test about the newly method
    'get_all_divisors_of_n_from_prime_factors".

--- tests/test_prime.c
+++ tests/test_prime.c
@@ -4,6 +4,14 @@
 #include "../src/math/prime.h"
 #include "../src/macros.h"
 
+static int cmp_long(const void *_d1, const void *_d2)
+{
+    const long *d1 = _d1;
+    const long *d2 = _d2;
+
+    return *d1 - *d2;
+}
+
 static void test_is_prime(void)
 {
     assert(is_prime(2));
@@ -240,6 +248,208 @@ static void test_get_all_primes_factors_of_n(void)
 }
 
 static void
+check_obtained_divisors_of_n(long n, const gv_t(uint64) *obtained_divisors,
+                             const gv_t(uint64) *expected_divisors)
+{
+    gv_for_each_pos(pos, obtained_divisors) {
+        ASSERT(gv_contains(expected_divisors, obtained_divisors->tab[pos],
+                       GV_DICHOTOMY_SEARCH, cmp_long),
+               "%ld is not in the expected divisors of %ld",
+               obtained_divisors->tab[pos], n);
+    }
+}
+
+static void test_get_all_divisors_of_n(void)
+{
+    gv_t(uint64) primes;
+    gv_t(primes_factors) primes_factors;
+    gv_t(uint64) obtained_divisors;
+    gv_t(uint64) expected_divisors;
+
+    gv_init_size(&primes, 200);
+    gv_init(&primes_factors);
+    gv_init(&obtained_divisors);
+    gv_init(&expected_divisors);
+
+    get_all_n_first_primes(100, &primes);
+
+    /* {{{ Test with 220 */
+    /* {{{ set expected_divisors */
+
+    gv_add(&expected_divisors, 2);
+    gv_add(&expected_divisors, 4);
+    gv_add(&expected_divisors, 5);
+    gv_add(&expected_divisors, 10);
+    gv_add(&expected_divisors, 11);
+    gv_add(&expected_divisors, 20);
+    gv_add(&expected_divisors, 22);
+    gv_add(&expected_divisors, 44);
+    gv_add(&expected_divisors, 55);
+    gv_add(&expected_divisors, 110);
+    gv_add(&expected_divisors, 220);
+
+    /* }}} */
+
+    get_all_primes_factors_of_n(220, &primes, &primes_factors);
+
+    ASSERT_EQUAL_INT((int)primes_factors.len, 3);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[0].prime, 2);
+    ASSERT_EQUAL_INT(primes_factors.tab[0].iteration, 2);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[1].prime, 5);
+    ASSERT_EQUAL_INT(primes_factors.tab[1].iteration, 1);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[2].prime, 11);
+    ASSERT_EQUAL_INT(primes_factors.tab[2].iteration, 1);
+
+    get_all_divisors_of_n_from_prime_factors(&primes_factors,
+                                             &obtained_divisors);
+    check_obtained_divisors_of_n(220, &obtained_divisors, &expected_divisors);
+
+    /* }}} */
+    /* {{{ Test with 284 */
+
+    gv_fast_clear(&primes_factors);
+    gv_fast_clear(&obtained_divisors);
+    gv_fast_clear(&expected_divisors);
+
+    /* {{{ set expected_divisors */
+
+    gv_add(&expected_divisors, 2);
+    gv_add(&expected_divisors, 4);
+    gv_add(&expected_divisors, 71);
+    gv_add(&expected_divisors, 142);
+    gv_add(&expected_divisors, 284);
+
+    /* }}} */
+
+    get_all_primes_factors_of_n(284, &primes, &primes_factors);
+
+    ASSERT_EQUAL_INT((int)primes_factors.len, 2);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[0].prime, 2);
+    ASSERT_EQUAL_INT(primes_factors.tab[0].iteration, 2);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[1].prime, 71);
+    ASSERT_EQUAL_INT(primes_factors.tab[1].iteration, 1);
+
+    get_all_divisors_of_n_from_prime_factors(&primes_factors,
+                                             &obtained_divisors);
+    check_obtained_divisors_of_n(284, &obtained_divisors, &expected_divisors);
+
+    /* }}} */
+    /* {{{ Test with 17640 */
+
+    gv_fast_clear(&primes_factors);
+    gv_fast_clear(&obtained_divisors);
+    gv_fast_clear(&expected_divisors);
+
+    /* {{{ set expected_divisors */
+
+    gv_add(&expected_divisors, 2);
+    gv_add(&expected_divisors, 3);
+    gv_add(&expected_divisors, 4);
+    gv_add(&expected_divisors, 5);
+    gv_add(&expected_divisors, 6);
+    gv_add(&expected_divisors, 7);
+    gv_add(&expected_divisors, 8);
+    gv_add(&expected_divisors, 9);
+    gv_add(&expected_divisors, 10);
+    gv_add(&expected_divisors, 12);
+    gv_add(&expected_divisors, 14);
+    gv_add(&expected_divisors, 15);
+    gv_add(&expected_divisors, 18);
+    gv_add(&expected_divisors, 20);
+    gv_add(&expected_divisors, 21);
+    gv_add(&expected_divisors, 24);
+    gv_add(&expected_divisors, 28);
+    gv_add(&expected_divisors, 30);
+    gv_add(&expected_divisors, 35);
+    gv_add(&expected_divisors, 36);
+    gv_add(&expected_divisors, 40);
+    gv_add(&expected_divisors, 42);
+    gv_add(&expected_divisors, 45);
+    gv_add(&expected_divisors, 49);
+    gv_add(&expected_divisors, 56);
+    gv_add(&expected_divisors, 60);
+    gv_add(&expected_divisors, 63);
+    gv_add(&expected_divisors, 70);
+    gv_add(&expected_divisors, 72);
+    gv_add(&expected_divisors, 84);
+    gv_add(&expected_divisors, 90);
+    gv_add(&expected_divisors, 98);
+    gv_add(&expected_divisors, 105);
+    gv_add(&expected_divisors, 120);
+    gv_add(&expected_divisors, 126);
+    gv_add(&expected_divisors, 140);
+    gv_add(&expected_divisors, 147);
+    gv_add(&expected_divisors, 168);
+    gv_add(&expected_divisors, 180);
+    gv_add(&expected_divisors, 196);
+    gv_add(&expected_divisors, 210);
+    gv_add(&expected_divisors, 245);
+    gv_add(&expected_divisors, 252);
+    gv_add(&expected_divisors, 280);
+    gv_add(&expected_divisors, 294);
+    gv_add(&expected_divisors, 315);
+    gv_add(&expected_divisors, 360);
+    gv_add(&expected_divisors, 392);
+    gv_add(&expected_divisors, 420);
+    gv_add(&expected_divisors, 441);
+    gv_add(&expected_divisors, 490);
+    gv_add(&expected_divisors, 504);
+    gv_add(&expected_divisors, 588);
+    gv_add(&expected_divisors, 630);
+    gv_add(&expected_divisors, 735);
+    gv_add(&expected_divisors, 840);
+    gv_add(&expected_divisors, 882);
+    gv_add(&expected_divisors, 980);
+    gv_add(&expected_divisors, 1176);
+    gv_add(&expected_divisors, 1260);
+    gv_add(&expected_divisors, 1470);
+    gv_add(&expected_divisors, 1764);
+    gv_add(&expected_divisors, 1960);
+    gv_add(&expected_divisors, 2205);
+    gv_add(&expected_divisors, 2520);
+    gv_add(&expected_divisors, 2940);
+    gv_add(&expected_divisors, 3528);
+    gv_add(&expected_divisors, 4410);
+    gv_add(&expected_divisors, 5880);
+    gv_add(&expected_divisors, 8820);
+    gv_add(&expected_divisors, 17640);
+
+    /* }}} */
+
+    get_all_primes_factors_of_n(17640, &primes, &primes_factors);
+
+    ASSERT_EQUAL_INT((int)primes_factors.len, 4);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[0].prime, 2);
+    ASSERT_EQUAL_INT(primes_factors.tab[0].iteration, 3);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[1].prime, 3);
+    ASSERT_EQUAL_INT(primes_factors.tab[1].iteration, 2);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[2].prime, 5);
+    ASSERT_EQUAL_INT(primes_factors.tab[2].iteration, 1);
+
+    ASSERT_EQUAL_INT((int)primes_factors.tab[3].prime, 7);
+    ASSERT_EQUAL_INT(primes_factors.tab[3].iteration, 2);
+
+    get_all_divisors_of_n_from_prime_factors(&primes_factors,
+                                             &obtained_divisors);
+    check_obtained_divisors_of_n(17640, &obtained_divisors,
+                                 &expected_divisors);
+
+    /* }}} */
+
+    gv_wipe(&primes, NULL);
+    gv_wipe(&primes_factors, NULL);
+    gv_wipe(&obtained_divisors, NULL);
+    gv_wipe(&expected_divisors, NULL);
+}
+
 static void
 get_and_check_divisors_count_of_n(const gv_t(uint64) *primes, unsigned int n,
                                    unsigned int expected_divisors_count)
@@ -336,6 +546,7 @@ module_tests_t *get_all_tests_prime(void)
     ADD_TEST_TO_MODULE(module_tests, test_get_all_primes_below_n);
     ADD_TEST_TO_MODULE(module_tests, test_get_all_n_first_primes);
     ADD_TEST_TO_MODULE(module_tests, test_get_all_primes_factors_of_n);
+    ADD_TEST_TO_MODULE(module_tests, test_get_all_divisors_of_n);
     ADD_TEST_TO_MODULE(module_tests, test_get_divisors_count);
     ADD_TEST_TO_MODULE(module_tests, test_get_phi_from_primes_factors);
     ADD_TEST_TO_MODULE(module_tests, test_get_all_phi_from_1_to_n);
