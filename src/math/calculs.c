8a70c47ed9db2fbc2135e0089dbf5fbce6cfb2af
LI <duleu.elie@gmail.com>
2025-12-09 03:40:11 +0100

    src/calculs: add 'get_closest_next_power_of_two'.
    Add the method 'get_closest_next_power_of_two' returning the newt power
    of 2. Careful, the maximal number here is a long.

--- src/math/calculs.c
+++ src/math/calculs.c
@@ -9,3 +9,20 @@ unsigned long sum_carre_1_to_n(unsigned long n)
 {
     return (n * (n + 1) * (2 * n + 1)) / 6;
 }
+
+long get_next_closest_power_of_two(long n)
+{
+    n--;
+
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n |= n >> 32;
+
+    n++;
+
+    return n;
+}
+
--- src/math/calculs.h
+++ src/math/calculs.h
@@ -4,4 +4,6 @@
 __attribute__((const)) unsigned long sum_1_to_n(unsigned long n);
 __attribute__((const)) unsigned long sum_carre_1_to_n(unsigned long n);
 
+__attribute__((const)) long get_next_closest_power_of_two(long n);
+
 #endif
