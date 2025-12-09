c26a8fd45fa28676e1a49df130f91652366193db
LI <duleu.elie@gmail.com>
2025-12-10 00:49:44 +0100

    src/big_numbers: add setting methods.
    Add the methods 'bn_set_from_l', 'bn_set_from_ul' and 'bn_set_from_bn'
    letting user set a big_number from a long, unsigned long or another big
    number.

--- src/math/big_numbers.c
+++ src/math/big_numbers.c
@@ -23,6 +23,54 @@ void bn_init(big_number_t *bn)
     bn_init_with_args(bn, 0, LIMIT_MAX);
 }
 
+/* {{{ setting methods */
+
+void bn_set_from_bn(const big_number_t *in, big_number_t *out)
+{
+    bn_fast_clear(out);
+
+    if (out->parts.size < in->parts.len) {
+        gv_extend(&(out->parts), (in->parts.len - out->parts.size));
+    }
+    memcpy(out->parts.tab, in->parts.tab,
+           in->parts.len * in->parts.__size_elem);
+
+    out->parts.len = in->parts.len;
+
+    out->positive_number = in->positive_number;
+    out->limit = in->limit;
+}
+
+void bn_set_from_ul(big_number_t *bn, unsigned long n)
+{
+    bn_fast_clear(bn);
+
+    while (n >= bn->limit) {
+        unsigned long carry = n / bn->limit;
+
+        n -= bn->limit * carry;
+        gv_add(&(bn->parts), n);
+
+        n = carry;
+    }
+    gv_add(&(bn->parts), n);
+}
+
+void bn_set_from_l(big_number_t *bn, long n)
+{
+    if (n > 0) {
+        bn_set_from_ul(bn, n);
+    } else {
+        bn_set_from_ul(bn, -n);
+    }
+
+    if (n < 0) {
+        bn->positive_number = false;
+    }
+}
+
+/* }}} */
+
 void bn_fast_clear(big_number_t *bn)
 {
     gv_fast_clear(&(bn->parts));
--- src/math/big_numbers.h
+++ src/math/big_numbers.h
@@ -12,6 +12,10 @@ typedef struct big_number_t {
 void bn_init(big_number_t *bn);
 void bn_init_with_args(big_number_t *bn, long size, unsigned long limit);
 
+void bn_set_from_bn(const big_number_t *in, big_number_t *out);
+void bn_set_from_ul(big_number_t *bn, unsigned long n);
+void bn_set_from_l(big_number_t *bn, long n);
+
 void bn_fast_clear(big_number_t *bn);
 void bn_clear(big_number_t *bn);
 void bn_wipe(big_number_t *bn);
