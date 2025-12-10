55fa5e09676e40a524b2c771f554ca0e0f67e1bb
LI <duleu.elie@gmail.com>
2025-12-10 18:36:49 +0100

    tests/big_numbers: factorize the code.
    Add a method to check the value of a big number and use it.

--- tests/test_big_numbers.c
+++ tests/test_big_numbers.c
@@ -4,12 +4,25 @@
 #include "../src/macros.h"
 #include "../src/mem/mem.h"
 
+static void
+check_bn_value_str(const big_number_t *bn, const char *expected_str)
+{
+    char *bn_str;
+
+    bn_str = bn_to_str(bn);
+
+    ASSERT(strcmp(bn_str, expected_str) == 0,
+           "\nbig numbers string expected: %s\n,"
+           "                  obtained: %s\n", expected_str, bn_str);
+
+    p_free((void **)&bn_str);
+}
+
 /* {{{ setting tests */
 
 static void test_bn_set_from_bn(void)
 {
     big_number_t bn, bn2;
-    char *obtained_str, *expected_str;
 
     bn_init(&bn);
     bn_init(&bn2);
@@ -23,18 +36,7 @@ static void test_bn_set_from_bn(void)
     ASSERT_EQUAL_LONG(bn.parts.tab[0], 2L);
     ASSERT_EQUAL_LONG(bn.parts.tab[1], 1L);
 
-    obtained_str = bn_to_str(&bn);
-
-    expected_str = p_calloc(19);
-    assert(expected_str != NULL);
-    snprintf(expected_str, 19, "100000000000000002");
-    ASSERT(strcmp(obtained_str, expected_str) == 0,
-           "\nbig numbers string expected: %s\n,"
-           "                  obtained: %s\n",
-           expected_str, obtained_str);
-
-    p_free((void **)&obtained_str);
-    p_free((void **)&expected_str);
+    check_bn_value_str(&bn, "100000000000000002");
 
     /* }}} */
     /* {{{ set bn to -6575901 */
@@ -45,42 +47,18 @@ static void test_bn_set_from_bn(void)
     ASSERT_EQUAL_LONG(bn2.parts.tab[0], 6575901L);
     ASSERT(!bn2.positive_number, "this big number is a negative one");
 
-    obtained_str = bn_to_str(&bn2);
-
-    expected_str = p_calloc(19);
-    assert(expected_str != NULL);
-    snprintf(expected_str, 19, "-6575901");
-    ASSERT(strcmp(obtained_str, expected_str) == 0,
-           "\nbig numbers string expected: %s\n,"
-           "                  obtained: %s\n",
-           expected_str, obtained_str);
-
-    p_free((void **)&obtained_str);
-    p_free((void **)&expected_str);
+    check_bn_value_str(&bn2, "-6575901");
 
     /* }}} */
     /* reset bn from bn2 */
 
     bn_set_from_bn(&bn2, &bn);
 
-    bn_set_from_l(&bn2, -6575901);
-
     ASSERT_EQUAL_LONG(bn2.parts.len, 1L);
     ASSERT_EQUAL_LONG(bn2.parts.tab[0], 6575901L);
     ASSERT(!bn2.positive_number, "this big number is a negative one");
 
-    obtained_str = bn_to_str(&bn2);
-
-    expected_str = p_calloc(19);
-    assert(expected_str != NULL);
-    snprintf(expected_str, 19, "-6575901");
-    ASSERT(strcmp(obtained_str, expected_str) == 0,
-           "\nbig numbers string expected: %s\n,"
-           "                  obtained: %s\n",
-           expected_str, obtained_str);
-
-    p_free((void **)&obtained_str);
-    p_free((void **)&expected_str);
+    check_bn_value_str(&bn, "-6575901");
 
     /* }}} */
 
@@ -91,7 +69,6 @@ static void test_bn_set_from_bn(void)
 static void test_bn_set_from_ul(void)
 {
     big_number_t bn;
-    char *obtained_str, *expected_str;
 
     bn_init_with_args(&bn, 0, 10000000);
 
@@ -106,18 +83,7 @@ static void test_bn_set_from_ul(void)
     ASSERT_EQUAL_LONG(bn.parts.tab[1], 0L);
     ASSERT_EQUAL_LONG(bn.parts.tab[2], 1000L);
 
-    obtained_str = bn_to_str(&bn);
-
-    expected_str = p_calloc(19);
-    assert(expected_str != NULL);
-    snprintf(expected_str, 19, "100000000000000002");
-    ASSERT(strcmp(obtained_str, expected_str) == 0,
-           "\nbig numbers string expected: %s\n,"
-           "                  obtained: %s\n",
-           expected_str, obtained_str);
-
-    p_free((void **)&obtained_str);
-    p_free((void **)&expected_str);
+    check_bn_value_str(&bn, "100000000000000002");
 
     /* }}} */
     /* {{{ Test with 10005228970000272820072000070002 */
@@ -129,18 +95,7 @@ static void test_bn_set_from_ul(void)
     ASSERT_EQUAL_LONG(bn.parts.tab[1], 605L);
     ASSERT_EQUAL_LONG(bn.parts.tab[2], 1000L);
 
-    obtained_str = bn_to_str(&bn);
-
-    expected_str = p_calloc(33);
-    assert(expected_str != NULL);
-    snprintf(expected_str, 33, "100000006050900972");
-    ASSERT(strcmp(obtained_str, expected_str) == 0,
-           "\nbig numbers string expected: %s\n,"
-           "                  obtained: %s\n",
-           expected_str, obtained_str);
-
-    p_free((void **)&obtained_str);
-    p_free((void **)&expected_str);
+    check_bn_value_str(&bn, "100000006050900972");
 
     /* }}} */
 
@@ -150,7 +105,6 @@ static void test_bn_set_from_ul(void)
 static void test_bn_set_from_l(void)
 {
     big_number_t bn;
-    char *obtained_str, *expected_str;
 
     bn_init(&bn);
 
@@ -162,18 +116,7 @@ static void test_bn_set_from_l(void)
     ASSERT_EQUAL_LONG(bn.parts.tab[0], 6575901L);
     ASSERT(!bn.positive_number, "this big number is a negative one");
 
-    obtained_str = bn_to_str(&bn);
-
-    expected_str = p_calloc(19);
-    assert(expected_str != NULL);
-    snprintf(expected_str, 19, "-6575901");
-    ASSERT(strcmp(obtained_str, expected_str) == 0,
-           "\nbig numbers string expected: %s\n,"
-           "                  obtained: %s\n",
-           expected_str, obtained_str);
-
-    p_free((void **)&obtained_str);
-    p_free((void **)&expected_str);
+    check_bn_value_str(&bn, "-6575901");
 
     /* }}} */
     /* {{{ Test with 10005228970000272820072000070002 */
@@ -185,18 +128,7 @@ static void test_bn_set_from_l(void)
     ASSERT_EQUAL_LONG(bn.parts.tab[1], 1L);
     ASSERT(!bn.positive_number, "bn should be a negative number");
 
-    obtained_str = bn_to_str(&bn);
-
-    expected_str = p_calloc(33);
-    assert(expected_str != NULL);
-    snprintf(expected_str, 33, "-100000006050900972");
-    ASSERT(strcmp(obtained_str, expected_str) == 0,
-           "\nbig numbers string expected: %s\n,"
-           "                  obtained: %s\n",
-           expected_str, obtained_str);
-
-    p_free((void **)&obtained_str);
-    p_free((void **)&expected_str);
+    check_bn_value_str(&bn, "-100000006050900972");
 
     /* }}} */
 
