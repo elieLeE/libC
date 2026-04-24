4198213b146e1a541de0cacd53aac8df83fec621
LI <duleu.elie@gmail.com>
2026-04-24 17:07:56 +0200

    test/bn: add test about 'bn_cmp'.

--- tests/test_big_numbers.c
+++ tests/test_big_numbers.c
@@ -36,6 +36,41 @@ static void test_bn_get_digits_count(void)
     ASSERT_EQUAL_INT(bn_get_digits_count(&bn), 3);
 }
 
+static void test_bn_cmp(void)
+{
+    big_number_t bn, bn2;
+
+    bn_init(&bn);
+    bn_init(&bn2);
+
+    bn_init_with_args(&bn, 0, 10000000);
+    bn_init_with_args(&bn2, 0, 10000000);
+
+    bn_set_from_ul(1768928368, &bn);
+    bn_set_from_ul(1768928367, &bn2);
+    assert((bn_cmp(&bn, &bn2) > 0));
+
+    bn_set_from_ul(1768928368, &bn);
+    bn_set_from_ul(2768928367, &bn2);
+    assert((bn_cmp(&bn, &bn2) < 0));
+
+    bn_set_from_ul(31768928368, &bn);
+    bn_set_from_ul(2768928367, &bn2);
+    assert((bn_cmp(&bn, &bn2) > 0));
+
+    bn_set_from_ul(99, &bn);
+    bn_set_from_ul(27689283678888, &bn2);
+    assert((bn_cmp(&bn, &bn2) < 0));
+
+    bn_set_from_ul(27689283678888, &bn);
+    bn_set_from_ul(27689283678888, &bn2);
+    assert((bn_cmp(&bn, &bn2) == 0));
+
+    bn_set_from_ul(2, &bn);
+    bn_set_from_ul(7, &bn2);
+    assert((bn_cmp(&bn, &bn2) < 0));
+}
+
 /* }}} */
 /* {{{ Setting tests */
 
@@ -204,6 +239,8 @@ module_tests_t *get_all_tests_big_numbers(void)
     set_module_name(module_tests, "BIG_NUMBERS");
 
     ADD_TEST_TO_MODULE(module_tests, test_bn_get_digits_count);
+
+    ADD_TEST_TO_MODULE(module_tests, test_bn_cmp);
 
     ADD_TEST_TO_MODULE(module_tests, test_bn_set_from_ul);
     ADD_TEST_TO_MODULE(module_tests, test_bn_set_from_l);
