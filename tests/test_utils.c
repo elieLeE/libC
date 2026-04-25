6eddb27510f6dcb4ea1d4c30970a1b7a7539218d
LI <duleu.elie@gmail.com>
2026-04-25 11:37:58 +0200

    test/utils: complete these tests.
    Adding the tests 'g_cmp_int32', 'g_cmp_uint32', 'g_cmp_int64' and
    'g_cmp_uint64'.

--- tests/test_utils.c
+++ tests/test_utils.c
@@ -18,12 +18,64 @@ static void test_swap(void)
     ASSERT_EQUAL_INT(b, 5);
 }
 
+static void test_g_cmp_int32(void)
+{
+    int n, n2;
+
+    n = 10;
+    n2 = INT_MAX;
+    assert(g_cmp_int32(&n, &n2) < 0);
+}
+
+static void test_g_cmp_uint32(void)
+{
+    unsigned int n, n2;
+
+    n = 10;
+    n2 = UINT_MAX;
+    assert(g_cmp_uint32(&n, &n2) < 0);
+}
+
+static void test_g_cmp_int64(void)
+{
+    long n, n2;
+
+    n = 10;
+    n2 = LONG_MAX;
+    assert(g_cmp_int64(&n, &n2) < 0);
+
+    n = 10;
+    n2 = 11;
+    assert(g_cmp_int64(&n, &n2) < 0);
+
+    n = 10000;
+    n2 = 11;
+    assert(g_cmp_int64(&n, &n2) > 0);
+
+    n = 11;
+    n2 = 11;
+    assert(g_cmp_int64(&n, &n2) == 0);
+}
+
+static void test_g_cmp_uint64(void)
+{
+    unsigned long n, n2;
+
+    n = 10;
+    n2 = ULONG_MAX;
+    assert(g_cmp_uint64(&n, &n2) < 0);
+}
+
 module_tests_t *get_all_tests_utils(void)
 {
     module_tests_t *module_tests = RETHROW_P(module_tests_new());
 
     set_module_name(module_tests, "UTILS");
     ADD_TEST_TO_MODULE(module_tests, test_swap);
+    ADD_TEST_TO_MODULE(module_tests, test_g_cmp_int32);
+    ADD_TEST_TO_MODULE(module_tests, test_g_cmp_uint32);
+    ADD_TEST_TO_MODULE(module_tests, test_g_cmp_int64);
+    ADD_TEST_TO_MODULE(module_tests, test_g_cmp_uint64);
 
     return module_tests;
 }
