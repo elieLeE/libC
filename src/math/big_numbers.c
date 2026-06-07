22320803452cc92d1ec9eb014b8dd438b192f040
LI <duleu.elie@gmail.com>
2026-06-07 19:15:20 +0200

    src/bn: improve 'bn_add_ul'.
    In this method, if "bn" is empty, out is set to "n".
    First, we would set the limit of "out" from the limit of "bn".

--- src/math/big_numbers.c
+++ src/math/big_numbers.c
@@ -440,17 +440,17 @@ static void _bn_sub_ul(const big_number_t *bn, uint64_t n, big_number_t *out)
 
 void bn_add_ul(const big_number_t *bn, uint64_t n, big_number_t *out)
 {
-    if (bn->parts.len == 0) {
-        bn_set_from_ul(n, out);
-        return;
-    }
-
     if (bn != out) {
         bn_fast_clear(out);
 
         if (bn->limit != out->limit) {
             bn_set_limit(out, bn->limit);
         }
+    }
+
+    if (bn->parts.len == 0) {
+        bn_set_from_ul(n, out);
+        return;
     }
 
     if (bn->positive_number) {
