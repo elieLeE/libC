a8e2d0dd1ed329c5646bae1d5c6fb4e677cc6f8c
leE <duleu.elie@gmail.com>
2026-09-02 21:43:24 +0200

    src/vector: move 'gv_t' declaration.
    Move this declaration in order to directly use it in 'generic_vector_t'
    MACRO.
    That will cancel eventual errors when changing one but not the other.

--- src/containers/vector.h
+++ src/containers/vector.h
@@ -29,18 +29,18 @@ typedef enum gv_algo_search_t {
 
 typedef generic_vector_data_t(void) __vector_void_t;
 
+#define gv_t(_name_) generic_vector_##_name_##_t
+
 /* len is the number of elements in the vector.
  * size if the size of the vector. Indeed, when elements are removed,
  * the array is not automatically reallocated.
  * We use a union with almost 2 same type in order to call some generics
  * methods giving to them __vector_void_t' when it will be possible. */
 #define generic_vector_t(_name, _type)                                        \
-    typedef union generic_vector_##_name##_t {                                \
+    typedef union gv_t(_name) {                                               \
         generic_vector_data_t(_type);                                         \
         __vector_void_t vec;                                                  \
-    } generic_vector_##_name##_t;
-
-#define gv_t(_name_) generic_vector_##_name_##_t
+    } gv_t(_name);
 
 #define __gv_size(_vec) sizeof(_vec->tab[0])
 #define __gv_type(_vec) typeof(_vec->tab[0])
